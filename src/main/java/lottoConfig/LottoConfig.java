492b76e8f622b81ad8cfe174f77b172c745cece9
karol-musial <musialkarol99@gmail.com>
2021-04-03 18:54:50 +0200

    Lotto

--- src/main/java/lottoConfig/LottoConfig.java
+++ src/main/java/lottoConfig/LottoConfig.java
@@ -4,7 +4,7 @@ public class LottoConfig {
 
     public static final int NUMBERS_TO_GENERATE = 6;
     public static final int LOWER_BOND = 1;
-    public static final int UPPER_BOND = 9;
+    public static final int UPPER_BOND = 99;
 
     private LottoConfig() {
     }
